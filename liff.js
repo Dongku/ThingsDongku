36dab5d5a84e45f234d2df705cb8983630b3e293
jaturongraIYC <dong_t49@hotmail.com>
2019-08-10 22:33:50 +0700

    Update liff.js
    update mybroad

--- liff.js
+++ liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'dfeffa9a-d8ee-4405-b94d-830623acd825'; // LED, Button
+const USER_SERVICE_UUID         = '79bec577-bce6-4b7e-8442-2ba660c6ea73'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
